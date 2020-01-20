02b459208f409bcc5c3c5dc7aa8bd0845557b3cf
unknown <mohamedamjoud@outlook.com>
2020-01-21 00:23:50 +0100

    variables & types

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -2,4 +2,34 @@
 //     console.log(message);
 // }
 // var message = 'Hello World';
-// log(message);
+// log(message);  
+var numver = 1;
+var count = 2;
+// es5 
+// es6(2015)
+// es2016
+// es2017
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    // console.log('Finally: ' +i);
+}
+doSomething();
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, 'a', false];
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.Red;
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -8,3 +8,31 @@
 
 var numver =1;
 let count =2; 
+
+// es5 
+// es6(2015)
+// es2016
+// es2017
+
+function doSomething(){
+    for (let i = 0; i<5;i++){
+        console.log(i);
+    }
+    // console.log('Finally: ' +i);
+}
+
+doSomething();
+
+let a : number;
+let b : boolean;
+let c : string;
+let d : any;
+let e : number []= [1,2,3];
+let f : any [] = [1,true,'a',false];
+
+const ColorRed =0;
+const ColorGreen =1;
+const ColorBlue =2;
+
+enum Color { Red=0, Green = 1, Blue =2};
+let backgroundColor = Color.Red;
